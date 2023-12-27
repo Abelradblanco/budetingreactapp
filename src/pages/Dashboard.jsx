8fa68f45c94f6f545de802dff144d3edd91e2b5e
abelradblanco <abelb200@gmail.com>
2023-12-27 16:42:24 -0600

    inital git

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { fetchData } from '../helpers'
+import { useLoaderData } from 'react-router-dom';
+import Intro from '../components/Intro';
+import { toast } from 'react-toastify';
+
+//loader function
+export function dashboardLoader(){
+    const userName =fetchData("userName");
+
+    return {userName}
+}
+
+//dashboard action
+export async function dashboardAction({request}){
+
+  const data = await request.formData();
+  const formData = Object.fromEntries(data)
+  try{
+    localStorage.setItem("userName", JSON.stringify(formData.userName))
+    return toast.success(`Welcome, ${formData.userName}`)
+  }catch(e){
+    throw new Error('There was a problem creating your account');
+  }
+}
+
+const Dashboard = () => {
+
+    const {userName} = useLoaderData();
+
+  return (
+    <div>
+        {userName ? (<p>{userName}</p>): 
+        <Intro />}
+    </div>
+  )
+}
+
+export default Dashboard
+
